8b9bc4d99d5e7fd5efeab1517ba479b45f29ce94
Nasir Ahmed <ahsannasirahmed@gmail.com>
2023-05-07 19:57:44 +0600

    first commit

---
+++ js/styleInjs.js
@@ -0,0 +1,8 @@
+const sections =document.querySelectorAll('section');
+for (const section of sections){
+    section.style.border= '2px solid skyblue';
+    section.style.marginBottom= '10px';
+    section.style.borderRadius= '10px';
+    section.style.paddingLeft= '10px';
+    section.style.backgroundColor= 'lightgray';
+}
